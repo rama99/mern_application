38287bb1dcf9684847e2309be83dc653c786b2f1
rama99 <rama.kishore@gmail.com>
2019-12-16 20:48:38 +0530

    Started adding routes and controllers for bootcamp resource

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const express = require(`express`);
+
+// route files
+const bootcamps = require(`./routes/bootcamps`);
+
 require(`dotenv`).config({path: `./config/config.env`}); // for environment variables
 
+const logger = require(`./middleware/logger`);
+const morgan = require(`morgan`);
+
 const app = express();
+
+// loggere in dev environment
+if(process.env.NODE_ENV === `development`) {
+    app.use(morgan(`dev`));
+}
+
+
+// Mount routers
+app.use(`/api/v1/bootcamps` , bootcamps);
+
+app.get("/" , (req , res) => {
+    res.sendStatus(400);
+})
 
 app.listen(process.env.PORT || 5000 , () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`)
