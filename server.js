45ac06d24b74b8008e5eb4dd981002a3f6c6a5b1
rama99 <rama.kishore@gmail.com>
2019-12-21 16:03:18 +0530

    Working on User related code model/route/controller

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ connectDB();
 // route files
 const bootcamps = require(`./routes/bootcamps`);
 const courses = require(`./routes/courses`);
+const auth = require(`./routes/auth`);
 
 const app = express();
 
@@ -34,6 +35,8 @@ app.use(express.static(path.join(__dirname,`public`)));
 // Mount routers
 app.use(`/api/v1/bootcamps` , bootcamps);
 app.use(`/api/v1/courses` , courses);
+app.use(`/api/v1/auth` , auth);
+
 
 app.get("/" , (req , res) => {
     res.sendStatus(400);
