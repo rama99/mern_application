e070fcea962e66ab80dc06edada3b5a255c47486
rama99 <rama.kishore@gmail.com>
2019-12-22 21:13:31 +0530

    completed review controller / route / CRUD code

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const bootcamps = require(`./routes/bootcamps`);
 const courses = require(`./routes/courses`);
 const auth = require(`./routes/auth`);
 const users = require(`./routes/users`);
+const reviews = require(`./routes/reviews`);
 
 const app = express();
 
@@ -41,7 +42,7 @@ app.use(`/api/v1/bootcamps` , bootcamps);
 app.use(`/api/v1/courses` , courses);
 app.use(`/api/v1/auth` , auth);
 app.use(`/api/v1/users` , users);
-
+app.use(`/api/v1/reviews` , reviews);
 
 app.get("/" , (req , res) => {
     res.sendStatus(400);
